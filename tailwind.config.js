63da46e0b9d73c21681c3e443f020732c5c7831f
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-03-04 14:39:27 +0900

    [fix]테일윈드 세팅 적용 및 라이트 테마 고정

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,72 @@ module.exports = {
   darkMode: 'media',
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      keyframes: {
+        flash: {
+          '25%, 40%': { opacity: '0' },
+          '50%': { opacity: '1' },
+          '75%': { opacity: '0' },
+        },
+        // fade
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(20px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        fadeOutRight: {
+          '0%': { opacity: '1', transform: 'translateX(0)' },
+          '100%': { opacity: '0', transform: 'translateX(20px)' },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeOutDown: {
+          '0%': { opacity: '1', transform: 'translateY(0)' },
+          '100%': { opacity: '0', transform: 'translateY(20px)' },
+        },
+
+        // bounce
+        bounceLeft: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(-20px)' },
+        },
+      },
+      animation: {
+        flash: 'flash 2s infinite',
+        fadeInRight: 'fadeInRight 1s ease-in-out',
+        fadeOutRight: 'fadeOutRight 1s ease-out',
+        fadeInUp: 'fadeInUp 1s ease-in-out',
+        fadeOutDown: 'fadeOutDown 1s ease-out',
+        bounceLeft: 'bounceLeft 2s infinite',
+      },
       colors: {
-        primary: '#591FD9',
+        primary: {
+          100: '#F9F5FF',
+          200: '#E2D7FF',
+          300: '#B39DDB',
+          400: '#C1ABF1',
+          500: '#7C4DFF',
+          600: '#591FD9',
+          700: '#616161',
+          800: '#424242',
+          900: '#212121',
+        },
+        'primary-gray': {
+          50: '#FAFAFA',
+          100: '#F5F5F5',
+          200: '#EEEEEE',
+          300: '#E0E0E0',
+          400: '#BDBDBD',
+          500: '#9E9E9E',
+          600: '#757575',
+          700: '#616161',
+          800: '#424242',
+          900: '#212121',
+        },
       },
       fontFamily: {
         Pretendard: ['Pretendard'],
